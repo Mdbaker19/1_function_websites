777fc2a129e73c29f94ae471e2ff3a3842e91c52
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-09-26 15:47:13 -0500

    sub function done

--- HTML/website2-calculator-attempt1/website2.js
+++ HTML/website2-calculator-attempt1/website2.js
@@ -1,14 +1,14 @@
-window.onload = function() {
-    const num1 = document.getElementById("firstNum");
-    const num2 = document.getElementById("secondNum");
-    const answer = document.getElementById("calculate");
-    const response = document.getElementById("totalOfNums");
+window.onload = function calcNums () {
+    var num1 = document.getElementById("firstNum");
+    var num2 = document.getElementById("secondNum");
+    var answer = document.getElementById("calculate");
+    var response = document.getElementById("totalOfNums");
 
 
-    answer.addEventListener("click", function() {
+    answer.addEventListener("click", function add () {
         var num1Value = parseInt(num1.value);
         var num2Value = parseInt(num2.value);
-        if(num1.value.length < 1 || num2.value.length < 1){
+        if(num1.value.length < 1 || num2.value.length < 1) {
             alert("enter a number into both boxes please");
     } else
     {
@@ -16,4 +16,20 @@ window.onload = function() {
         response.innerText = sumOfNums;
     }
     });
+
+    var subNum1 = document.getElementById("firstSubNum");
+    var subNum2 = document.getElementById("secondSubNum");
+    var subAnswer = document.getElementById("calcSub");
+    var subResponse = document.getElementById("differenceOfNums");
+
+    subAnswer.addEventListener("click", function sub() {
+        var num1Value = parseInt(subNum1.value);
+        var num2Value = parseInt(subNum2.value);
+        if(subNum1.value.length < 1 || subNum2.value.length < 1) {
+            alert("enter a number into both boxes please");
+        } else {
+            var diffOfNums = (num1Value - num2Value);
+            subResponse.innerText = diffOfNums;
+        }
+    })
 };
