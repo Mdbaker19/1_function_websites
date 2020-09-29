2d3c217e5b783b3042dcc8babbc7966317f7ce42
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-09-29 13:21:11 -0500

    created a mouse click counter with a doubler function

---
+++ javascript/website6.js
@@ -0,0 +1,18 @@
+(function (){
+    window.onload = function startClicks () {
+        var clickCount = document.getElementById("clicks");
+        var numDisplay = document.getElementById("clickNum");
+        var double = document.getElementById("doubler");
+        var count = 0;
+
+        clickCount.addEventListener("click", function plusCount(){
+           count++;
+            numDisplay.innerText = count;
+        });
+
+        double.addEventListener("click", function doubleIt(){
+            count *= 2;
+            numDisplay.innerText = count;
+        })
+    }
+})();
