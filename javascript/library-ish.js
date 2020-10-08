c9edf84aaa7baab7cd6783b64f7c61b796c89b19
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-08 14:17:27 -0500

    trying to create more of a website from the objects books exercise

---
+++ javascript/library-ish.js
@@ -0,0 +1,41 @@
+(function (){
+    const books = [{
+        bookNum: 1,
+        title: "Elon Musk",
+        author: {
+            firstName: "Ashley",
+            lastName: "Vance"
+        },
+    }, {
+        bookNum: 2,
+        title: "Steal like an artist",
+        author: {
+            firstName: "Austin",
+            lastName: "Kleon"
+        },
+    }, {
+        bookNum: 3,
+        title: "How to win friends and influence people",
+        author: {
+            firstName: "Dale",
+            lastName: "Carnigie"
+        },
+    }, {
+        bookNum: 4,
+        title: "Think and grow rich",
+        author: {
+            firstName: "Napoleon",
+            lastName: "Hill"
+        },
+    }, {
+        bookNum: 5,
+        title: "Power trip",
+        author: {
+            firstName: "I do not",
+            lastName: "remember"
+        }
+    }]
+
+
+
+})();
